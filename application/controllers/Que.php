928687d9fdda0acb7b0f05efc0c061975f1c57cd
maxconsul17 <mconsul@schools.ph>
2024-09-23 10:14:32 +0800

    df

--- application/controllers/Que.php
+++ application/controllers/Que.php
@@ -78,7 +78,7 @@ class Que extends CI_Controller {
 
 		// FOR CALCULATING ATTENDANCE
 		$current_time = date('H:i:s');
-		if ($current_time === '11:00:00' || $current_time === '12:00:00' || $current_time === '13:00:00' || $current_time === '14:00:00') {
+		if ($current_time === '14:00:00') {
 			// Action to perform if the current time is 12:00:00
 			$this->calculateAttendance();
 		} 
